0af0e65633ef4f731c3468a9a539f96c0fa0f1f1
Yohann <yhzhang2@telenavsoftware.com>
2025-02-14 14:41:19 +0800

    add some packages.

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -3,3 +3,5 @@
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
+
+sed -i 's/^HOSTNAME="FriendlyWrt"/HOSTNAME="Router"/' friendlywrt/target/linux/rockchip/armv8/base-files/root/setup.sh
